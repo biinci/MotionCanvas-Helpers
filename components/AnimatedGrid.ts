7768c6d3fa5924b8936e666cf9ba6a0b4a3d486b
bilalinci <48059303+biinci@users.noreply.github.com>
2023-02-11 15:02:53 +0300

    changed animation style

--- components/AnimatedGrid.ts
+++ components/AnimatedGrid.ts
@@ -7,6 +7,7 @@ import {useContext, useLogger} from '@motion-canvas/core/lib/utils';
 export class AnimatedGrid extends Grid{
   protected drawing = false;
 
+
   protected override drawShape(context: CanvasRenderingContext2D): void {
 
     if(this.drawing){
@@ -36,21 +37,19 @@ export class AnimatedGrid extends Grid{
     }    
   }
   
-  public *play(){
+  public *play(eachRowDuration : number, eachColumDuration : number){
     this.drawing = false;
 
     const spacing = this.spacing();
     const size = this.computedSize().scale(0.5);
     const steps = size.div(spacing).floored;
-    const rot = this.rotation();
-    useLogger().info(rot.toString())
     
     const xAxis : SimpleSignal<number, void>[] =[]
     const yAxis : SimpleSignal<number, void>[] =[]
 
 
-    for (let x = -steps.x; x <= steps.x; x++) yAxis.push(createSignal(0));
-    for (let y = -steps.y; y <= steps.y; y++) xAxis.push(createSignal(0));
+    for (let x = -steps.x; x <= steps.x; x++) yAxis.push(createSignal(-size.height));
+    for (let y = -steps.y; y <= steps.y; y++) xAxis.push(createSignal(-size.width));
     
 
     useContext(context => {
@@ -58,17 +57,19 @@ export class AnimatedGrid extends Grid{
       context.save();
       this.applyStyle(context);
       this.drawRipple(context);
+      
+      
   
       for (let x = -steps.x; x <= steps.x; x++) {
         context.beginPath();
         context.moveTo(spacing.x * x, -yAxis[x + steps.x]());
-        context.lineTo(spacing.x * x, yAxis[x + steps.x]());
+        context.lineTo(spacing.x * x, size.height);
         context.stroke();
       }
   
       for (let y = -steps.y; y <= steps.y; y++) {
 
-        context.moveTo(-xAxis[y + steps.y](), spacing.y * y);
+        context.moveTo(-size.width, spacing.y * y);
         context.lineTo(xAxis[y + steps.y](), spacing.y * y);
 
         context.stroke();
@@ -79,12 +80,14 @@ export class AnimatedGrid extends Grid{
 
     yield* all(
       chain(
+
         yAxis[steps.x](size.height, 0.5),
+        xAxis[steps.y](size.width, 0.5),
         all(
-          loop(steps.y, i => xAxis[i + steps.y](size.width)),
-          loop(steps.y, i => xAxis[steps.y - i](size.width)),
-          loop(steps.x, i => yAxis[i + steps.x](size.height)),
-          loop(steps.x, i => yAxis[steps.x - i](size.height))
+          loop(steps.y, i => xAxis[i +1 + steps.y](size.width, eachRowDuration)),//rows down
+          loop(steps.y, i => xAxis[steps.y - 1 - i](size.width, eachRowDuration)),//rows up
+          loop(steps.x, i => yAxis[i + steps.x](size.height, eachColumDuration)),//colums right
+          loop(steps.x, i => yAxis[steps.x - i](size.height, eachColumDuration))//colums left
         )
           
       ),
